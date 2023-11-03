b7629002da3260c33dc1dc8ea577f229105a7a80
puxxbu <pusswarog@gmail.com>
2023-11-04 01:17:25 +0700

    back to 3000

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { web } from "./app/web.js";
 import { logger } from "./app/logging.js";
 
-web.listen(8080, "127.0.0.1", () => {
+web.listen(3000, "127.0.0.1", () => {
   logger.info("App start");
 });
